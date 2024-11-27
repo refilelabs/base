c4fb3025ebfcf86e3cdee0bdb7c8dad693bbc9f7
Tom Voet <tomvoet@gmx.de>
2024-11-27 17:21:29 +0100

    chore: further increased input modularity

--- components/Inputs/Image.vue
+++ components/Inputs/Image.vue
@@ -5,18 +5,11 @@ const props = withDefaults(defineProps<FileInputProps>(), fileInputPropsDefaults
 
 const data = useVModel(props, 'modelValue')
 
-const imgSource = computed(() => {
-  if (data.value) {
-    return URL.createObjectURL(data.value)
-  }
-  else {
-    return undefined
-  }
-})
+const imgSource = computed(() => data.value ? URL.createObjectURL(data.value) : undefined)
 </script>
 
 <template>
-  <InputsFile v-bind="props" v-model="data" accept="image/*">
+  <InputsFile v-bind="props" v-model="data" :accept="props.accept ?? 'image/*'">
     <template #file-preview>
       <img :src="imgSource || ''" class="object-scale-down max-h-56 max-w-xl min-h-24 min-w-24">
     </template>
