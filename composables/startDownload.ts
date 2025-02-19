ce3266ce1d13a6f503090cad1f967f056722df57
Tom Voet <tomvoet@pm.me>
2025-02-19 19:53:14 +0100

    feat: add composable to download from file object

--- composables/startDownload.ts
+++ composables/startDownload.ts
@@ -1,10 +1,20 @@
-export default function startDownload(file: Uint8Array, filename: string) {
+export function startDownload(file: Uint8Array, filename: string) {
   const blob = new Blob([file], { type: 'application/octet-stream' })
 
   const url = URL.createObjectURL(blob)
   const a = document.createElement('a')
   a.href = url
   a.download = filename
+  a.click()
+  a.remove()
+  URL.revokeObjectURL(url)
+}
+
+export function startDownloadFile(file: File) {
+  const url = URL.createObjectURL(file)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = file.name
   a.click()
   a.remove()
   URL.revokeObjectURL(url)
