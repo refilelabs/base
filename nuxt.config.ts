05bf2de470ff032ea919741182b4d14441d2874f
Tom Voet <tomvoet@pm.me>
2026-03-12 12:39:06 +0100

    feat: add vite-plugin-wasm to package.json and configure in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import wasm from 'vite-plugin-wasm'
 
 export const layerName = 'image'
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  vite: {
+    plugins: [wasm()],
+  },
   modules: ['@vueuse/nuxt', '@nuxt/ui', '@nuxt/eslint'],
   css: [`${currentDir}/assets/css/main.css`],
   eslint: {
