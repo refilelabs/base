4fc456b8561b67e7bddc51be7c971b20e616ff8c
Tom Voet <tomvoet@pm.me>
2026-03-16 00:16:47 +0100

    fix: remove vite-plugin-wasm dependency output type bundler doesnt seem to work in workers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import wasm from 'vite-plugin-wasm'
 
 export const layerName = 'image'
 const currentDir = dirname(fileURLToPath(import.meta.url))
@@ -8,9 +7,6 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  vite: {
-    plugins: [wasm()],
-  },
   modules: ['@vueuse/nuxt', '@nuxt/ui', '@nuxt/eslint', '@nuxt/fonts'],
   css: [`${currentDir}/assets/css/main.css`],
   eslint: {
